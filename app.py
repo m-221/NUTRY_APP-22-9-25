408121bd6bb4539ea07f3f5e60a503edfab1e774
m-221 <24308060610066@cetis61.edu.mx>
2025-11-04 21:18:16 -0700

    mel

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,flash
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('inico.html')
+    return render_template('inicio.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
